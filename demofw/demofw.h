d27d78535e4ce15596182d4db717dda9cb8de9e3
ITD 110 <ITD110@itd-106.local>
2016-08-05 08:55:31 +0700

    Initial Commit

---
+++ demofw/demofw.h
@@ -0,0 +1,19 @@
+//
+//  demofw.h
+//  demofw
+//
+//  Created by ITD_110 on 8/5/16.
+//  Copyright © 2016 ITD_110. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for demofw.
+FOUNDATION_EXPORT double demofwVersionNumber;
+
+//! Project version string for demofw.
+FOUNDATION_EXPORT const unsigned char demofwVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demofw/PublicHeader.h>
+
+
