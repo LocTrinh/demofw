bfc944103a160bcd516fbcf5c0d54c892d60fbeb
LocTrinh <trinhbaloc@gmail.com>
2016-08-05 10:24:52 +0700

    init

---
+++ demofw/HelloViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HelloViewController.swift
+//  demofw
+//
+//  Created by ITD_110 on 8/5/16.
+//  Copyright © 2016 ITD_110. All rights reserved.
+//
+
+import UIKit
+
+class HelloViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func xinchao(sender: AnyObject) {
+        print("xin chao")
+    }
+
+    @IBOutlet weak var xinchaothegioi: UIButton!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ demofw/Test.swift
@@ -0,0 +1,16 @@
+//
+//  Test.swift
+//  demofw
+//
+//  Created by ITD_110 on 8/5/16.
+//  Copyright © 2016 ITD_110. All rights reserved.
+//
+
+import Foundation
+
+
+public class demo3 {
+    public static func test2() -> Void {
+        print(#function)
+    }
+}
